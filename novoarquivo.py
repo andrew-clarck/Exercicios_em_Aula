95c75d0c2f9d094fd75f18996bb59b6c88928c8c
Andrew Clarck <andrew.clarck@aluno.senai.br>
2025-04-02 15:13:44 -0300

    Update novoarquivo.py
    atualizado texto sobre o Justin

--- novoarquivo.py
+++ novoarquivo.py
@@ -1,2 +1,2 @@
-print("O Justin é o melhor jogador de jogos de luta")
-print("O Justin é a pessoa mais azarada que eu conheço")
+print("O Justin é o PIOR jogador de jogos de luta que eu já vi")
+print("O Justin é sortudo")
